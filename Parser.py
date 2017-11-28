26cd317e4ad3cb252fb0ca8551e03f1f7d143c0b
Gabriel Secula <gabe.secula@gmail.com>
2017-11-28 16:30:23 -0500

    Update Parser.py
    Updated file to work with actual table data.

--- Parser.py
+++ Parser.py
@@ -1,4 +1,7 @@
 with open('example.txt', 'r') as f, open('updatedFile.txt', 'w') as n:
     for line in f:
         data = line.strip()
-        n.write('INSERT INTO' + data + ';\r\n')
+        if(data.substring[0,2] == '--')
+            currTable = data.substring[2:]
+        else
+            n.write('INSERT INTO '+currTable+' VALUES (' + data + ');\r\n')
