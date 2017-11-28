caa0a974f126eadc97b8096a1dd3cd2860a811eb
Gabriel Secula <gabe.secula@gmail.com>
2017-11-28 16:49:01 -0500

    Update Parser.py
    Should be cleaner now, just have to test when I get home.

--- Parser.py
+++ Parser.py
@@ -1,7 +1,9 @@
-with open('example.txt', 'r') as f, open('updatedFile.txt', 'w') as n:
+with open('tableData', 'r') as f, open('updatedFile.txt', 'w') as n:
     for line in f:
         data = line.strip()
         if(data.substring[0,2] == '--')
             currTable = data.substring[2:]
+            line++
+            columns = line.strip()
         else
-            n.write('INSERT INTO '+currTable+' VALUES (' + data + ');\r\n')
+            n.write('INSERT INTO '+currTable+'(' +columns+ ') VALUES (' + data + ');\r\n')
