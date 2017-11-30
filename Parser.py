5fa28d12fa5507a937081fa5fe9f4d6bb7544f9b
gabeSecula <gabe.secula@gmail.com>
2017-11-30 16:03:43 -0500

    IT'S ALIVE. Properly parses now.

--- Parser.py
+++ Parser.py
@@ -1,9 +1,11 @@
 with open('tableData', 'r') as f, open('updatedFile.txt', 'w') as n:
     for line in f:
         data = line.strip()
-        if(data.substring[0,2] == '--')
-            currTable = data.substring[2:]
-            line += 1 #might do readline instead
+        if(data[:2] == '--'):
+            currTable = data[2:]
+            line = f.readline();
             columns = line.strip()
-        else
+        else:
             n.write('INSERT INTO '+currTable+'(' +columns+ ') VALUES (' + data + ');\r\n')
+
+print('File Parsed');
