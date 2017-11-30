f87fd039fb8dcd13e45b95716bc4e89edc116196
Gabriel Secula <gabe.secula@gmail.com>
2017-11-30 12:47:42 -0500

    Update Parser.py
    Changes ++ to += 1, I still am not too familiar with Python.

--- Parser.py
+++ Parser.py
@@ -3,7 +3,7 @@ with open('tableData', 'r') as f, open('updatedFile.txt', 'w') as n:
         data = line.strip()
         if(data.substring[0,2] == '--')
             currTable = data.substring[2:]
-            line++
+            line += 1 #might do readline instead
             columns = line.strip()
         else
             n.write('INSERT INTO '+currTable+'(' +columns+ ') VALUES (' + data + ');\r\n')
